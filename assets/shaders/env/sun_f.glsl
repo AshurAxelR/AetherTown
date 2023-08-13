50e78b43bab42df1eb7e73eb94b7de227f9db9f4
Ashur R <7903175+AshurAxelR@users.noreply.github.com>
2023-08-13 23:29:06 +0100

    Fixed fun in fog

--- assets/shaders/env/sun_f.glsl
+++ assets/shaders/env/sun_f.glsl
@@ -1,6 +1,11 @@
 #version 150 core
 
+uniform vec3 cameraPosition;
+
 uniform vec4 lightColor = vec4(1, 0.949, 0.850, 1);
+
+uniform float cloudTop = 0;
+uniform float cloudBottom = -40;
 
 in vec3 pass_Position;
 in vec2 pass_TexCoord;
@@ -24,8 +29,9 @@ void main(void) {
 		d -= hdot*10;
 	
 	if(d>=1) discard;
-	float a = min(2, 10*(1-d)/(100*d+1));
+	float a = min(1.5, 10*(1-d)/(100*d+1));
+	float clear = 1 - clamp((cameraPosition.y-cloudTop)/(cloudBottom-cloudTop), 0, 1);
 	// out_Color = pow4(lightColor, 2.75) * 2.75;
 	float len = pow(length(lightColor.xyz), 0.35);
-	out_Color = vec4(lightColor.xyz*1.2/len + pow(max(0, (a-1)), 1.5), sqrt(a));
+	out_Color = vec4(lightColor.xyz*1.2/len + pow(max(0, (a*clear-1)), 1.5), sqrt(a)*clear);
 }
