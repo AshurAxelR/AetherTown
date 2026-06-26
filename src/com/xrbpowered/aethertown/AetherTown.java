a6a7ba61c80ad9bb9f7b045439fc2d000ba4a859
Ashur R <7903175+AshurAxelR@users.noreply.github.com>
2026-06-26 23:04:06 +0100

    Version a.0.3.5

--- src/com/xrbpowered/aethertown/AetherTown.java
+++ src/com/xrbpowered/aethertown/AetherTown.java
@@ -75,7 +75,7 @@ import com.xrbpowered.zoomui.MouseInfo;
 
 public class AetherTown extends UIClient {
 
-	public static final String version = "a.0.3.5-dev1.10";
+	public static final String version = "a.0.3.5";
 	
 	public static final float pawnHeight = 1.55f;
 	
